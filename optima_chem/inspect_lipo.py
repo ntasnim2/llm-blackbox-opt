da1fb641758cc30a620fe682f1e00c989a264bed
Naima <ntasnim2@asu.edu>
2026-06-17 15:33:11 -0700

    Update Jun 17 LIPO Dataset

--- optima_chem/inspect_lipo.py
+++ optima_chem/inspect_lipo.py
@@ -1,26 +1,36 @@
 import deepchem as dc
+import pandas as pd
 
-tasks, datasets, transformers = dc.molnet.load_lipo(
+tasks, data, transformers = dc.molnet.load_lipo(
     featurizer="ECFP",
     splitter="scaffold",
     transformers=["normalization"],
     reload=True,
 )
 
-train, valid, test = datasets
+# train, valid, test = datasets # UNNECESSARY for blackbox setup
+dataset = data[0]
 
 print("tasks:", tasks)
-print("sizes:", len(train), len(valid), len(test))
-print("X:", train.X.shape)
-print("y:", train.y.shape)
-print("ids:", train.ids.shape)
+print("sizes:", len(dataset))
+print("X:", dataset.X.shape)
+print("y:", dataset.y.shape)
+print("ids:", dataset.ids.shape)
 
-y_raw = transformers[0].untransform(train.y)
+y_raw = transformers[0].untransform(dataset.y)
 
 for i in range(10):
     print({
         "i": i,
-        "smiles": train.ids[i],
-        "y_normalized": float(train.y[i][0]),
+        "smiles": dataset.ids[i],
+        "y_normalized": float(dataset.y[i][0]),
         "y_raw_logD": float(y_raw[i][0]),
     })
+
+candidate_pool = pd.DataFrame({
+    "candidate_id": range(len(dataset.ids)),
+    "smiles": dataset.ids,
+    "logD": y_raw[:, 0],
+})
+
+print(candidate_pool.head())
